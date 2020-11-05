d79220ebcde8951b8c298208f12e4488d6906b40
yuzhe <yuzhe.hao.cn@gmail.com>
2020-11-06 00:33:12 +0900

    colorful!
    make the menu colorful

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function magic(target,xx,yy)
 	$(document).ready(function(){
 		$('body').mousemove(function(e) {
 	        var x = (e.pageX * -1 / 2), y = (e.pageY * -1 / 2);
-				$('div.'+target).css('left', (x/20)+xx+'px');
-				$('div.'+target).css('top', (y/40)+yy+'px');
+				$('div.'+target).css('left', (x/10)+xx+'px');
+				$('div.'+target).css('top', (y/10)+yy+'px');
 	   });
 	});
 }
