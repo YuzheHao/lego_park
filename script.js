1109032b6cd5d5ac69d8c0752b327c6d34b78487
yuzhe <yuzhe.hao.cn@gmail.com>
2020-11-05 12:05:27 +0900

    add menu block
    using hover, when your mouse on it, it will change to another style

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-function magic(target)
+function magic(target,xx,yy)
 {
 	$(document).ready(function(){
 		$('body').mousemove(function(e) {
 	        var x = (e.pageX * -1 / 2), y = (e.pageY * -1 / 2);
-				$('div.'+target).css('left', (x/20)+200+'px');
-				$('div.'+target).css('top', (y/40)+200+'px');
+				$('div.'+target).css('left', (x/20)+xx+'px');
+				$('div.'+target).css('top', (y/40)+yy+'px');
+	   });
+	});
+}
+
+function super_magic()
+{
+	$(document).ready(function(){
+		$('body').mousemove(function(e) {
+	        var x = (e.pageX * -1 / 2), y = (e.pageY * -1 / 2);
+				$('body').css('left', (x/20)+xx+'px');
+				$('body').css('top', (y/40)+yy+'px');
 	   });
 	});
 }
