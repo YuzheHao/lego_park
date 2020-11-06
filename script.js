004391e1f30850538f6b362b3c9a13dd87c9640a
yuzhe <yuzhe.hao.cn@gmail.com>
2020-11-07 00:45:41 +0900

    add auto aligned tool
    modified a lot about sublime

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function magic(target,xx,yy)
-{
-	$(document).ready(function(){
-		$('body').mousemove(function(e) {
-	        var x = (e.pageX * -1 / 2), y = (e.pageY * -1 / 2);
-				$('div.'+target).css('left', (x/10)+xx+'px');
-				$('div.'+target).css('top', (y/10)+yy+'px');
-	   });
-	});
+function magic(target, xx, yy) {
+    $(document).ready(function() {
+        $('body').mousemove(function(e) {
+            var x = (e.pageX * -1 / 2),
+                y = (e.pageY * -1 / 2);
+            $('div.' + target).css('left', (x / 10) + xx + 'px');
+            $('div.' + target).css('top', (y / 10) + yy + 'px');
+        });
+    });
 }
