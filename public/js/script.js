9a43bcff5e3523ea0c55ace670720001e7f6dd5b
Yuzhe <yuzhe.hao.cn@gmail.com>
2022-08-06 12:10:28 +0900

    cm2

--- public/js/script.js
+++ public/js/script.js
@@ -23,3 +23,12 @@ function change_text(target) {
             });
     });
 }
+// mi
+// 然后我得让git graph里的图合并过来
+
+// 是不是squash and merge了就连不起来了？
+// 我这次试试不squash，直接merge
+
+// 本地试试squash，这样相当于放弃了github那边的操作
+// 我其实也没有非要在那边使用的理由
+// 我的最终目的是保证master这个branch干净就可以了
