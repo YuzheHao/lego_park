339d78c1d79d83751c1ff413ea853ae47a980d9f
Yuzhe <yuzhe.hao.cn@gmail.com>
2022-08-10 20:34:47 +0900

    1

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,7 @@
 function magic(target, xx, yy) {
     $(document).ready(function() {
         $('body').mousemove(function(e) {
-            var x = (e.pageX * -1 / 2),
+            var x = (e.pageX *ΩΩ -1 / 2),
                 y = (e.pageY * -1 / 2);
             $('div.' + target).css('left', (x / 10) + xx + 'px');
             $('div.' + target).css('top', (y / 10) + yy + 'px');
